52ddd92a7c10139a5c0b1ec6914ee046bca23a95
pelmeshek2323 <ggmiha25@gmail.com>
2025-04-25 11:59:24 +0300

    free soa.lua

--- free soa.lua
+++ free soa.lua
@@ -1,4 +1,4 @@
-ocal Players = game:GetService("Players")
+local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local RunService = game:GetService("RunService")
 local UIS = game:GetService("UserInputService")
